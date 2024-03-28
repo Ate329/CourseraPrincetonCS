388ca205181342f3faac35a5fb2376422c96e446
Ate329 <justin.he814@gmail.com>
2024-03-28 16:44:40 +0200

    fixed style issue

--- week2/RandomWalker.java
+++ week2/RandomWalker.java
@@ -1,36 +1,40 @@
-public class RandomWalker {
-    public static void main(String[] args) {
-        long n = Long.parseLong(args[0]);
+public class RandomWalker
+{
+    public static void main(String[] args)
+    {
+        int n = Integer.parseInt(args[0]);
         
-        long x = 0;
-        long y = 0;
+        int x = 0;
+        int y = 0;
         int counter = 0; 
         long dist = 0;
         
-        while (dist != n){
+        while (dist < n)
+        {
             double prob = Math.random();
 
-            if (prob <= 0.25){
+            if (prob <= 0.25)
+            {
                 x += 1;
             }
-            else if (prob > 0.25 && prob <= 0.5){
+            else if (prob <= 0.5)
+            {
                 x += -1;
             }
-            else if (prob > 0.5 && prob <= 0.75){
+            else if (prob <= 0.75)
+            {
                 y += 1;
             }
-            else if (prob > 0.75 && prob <= 1.00){
+            else if (prob <= 1.00)
+            {
                 y += -1;
             }
             System.out.println("(" + x + ", " + y + ")");
 
-            dist = Math.abs(x) + Math.abs(y);
+            dist = Math.abs(x) + (long) Math.abs(y);
 
             counter++;
         }
-
         System.out.println("steps = " + counter);
-
     }
-    
 }
--- week2/RandomWalkers.java
+++ week2/RandomWalkers.java
@@ -1,36 +1,43 @@
-public class RandomWalkers {
-    public static void main(String[] args) {
+public class RandomWalkers
+{
+    public static void main(String[] args)
+    {
         int r = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
         
-        long x = 0;
-        long y = 0;
+        int x = 0;
+        int y = 0;
         int counter = 0; 
         long dist = 0;
         long total = 0;
         
-        for (int i = 0; i <= trials; i++){
-            while (dist != r){
+        for (int i = 0; i <= trials; i++)
+        {
+            while (dist < r)
+            {
                 double prob = Math.random();
 
-                if (prob <= 0.25){
+                if (prob <= 0.25)
+                {
                     x += 1;
                 }
-                else if (prob > 0.25 && prob <= 0.5){
+                else if (prob <= 0.5)
+                {
                     x += -1;
                 }
-                else if (prob > 0.5 && prob <= 0.75){
+                else if (prob <= 0.75)
+                {
                     y += 1;
                 }
-                else if (prob > 0.75 && prob <= 1.00){
+                else if (prob <= 1.00)
+                {
                     y += -1;
                 }
 
-                dist = Math.abs(x) + Math.abs(y);
+                dist = Math.abs(x) + (long) Math.abs(y);
 
                 counter++;
             }
-
             total = total + counter;
         }
         double average = total / (double) trials;
